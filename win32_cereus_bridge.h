2b78299ef5da76cacf0664c664df9e7245a93448
aeSpike <spike.z.vitalis@gmail.com>
2025-10-04 22:42:05 +0200

    remove duplicate files

--- win32_cereus_bridge.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "types.h"
-#include "worldstate_structs.h"
-
-void gameInitialise(void);
-void gameFrame(double, TickInput);
-void rendererSubmitFrame(TextureToLoad textures_to_load[128]);
-void rendererDraw();
